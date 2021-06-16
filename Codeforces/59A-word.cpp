ba805d0bda177e71b4a16a8f80ecf9d03ad6a2ad
SusmoySenGupta <susmoy.cse@gmail.com>
2021-06-17 01:18:13 +0600

    add 59A solution

---
+++ Codeforces/59A-word.cpp
@@ -0,0 +1,32 @@
+/* 
+	problem no: 59A
+	problem name: Word
+	problem link: https://codeforces.com/problemset/problem/59/A
+	author: Susmoy Sen Gupta
+	Status: __Solved__
+	Solved at: _Jun/17/2021 01:11_
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+    char str[105];
+    int countSmall = 0;
+    
+	cin >> str;
+    
+	for(int i = 0; i < strlen(str); i++){
+		if(islower(str[i]))
+			countSmall++;
+	}
+	
+	if(countSmall < strlen(str) - countSmall) 
+		cout << strupr(str) << endl;
+	
+	else
+		cout << strlwr(str) << endl;
+    
+    return 0;
+}
