dad114a78b662d89f8719fad801811b83482c4f6
SusmoySenGupta <susmoy.cse@gmail.com>
2021-06-17 11:36:56 +0600

    add 617A solution

---
+++ Codeforces/617A-elephant.cpp
@@ -0,0 +1,21 @@
+/* 
+	problem no: 617A
+	problem name: Elephant
+	problem link: https://codeforces.com/problemset/problem/617/A
+	author: Susmoy Sen Gupta
+	Status: __Solved__
+	Solved at: Jun/17/2021 11:32
+*/
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int x;
+    
+	cin >> x;
+    
+	cout << (x + 4) / 5 << endl;
+	
+	return 0;
+}
