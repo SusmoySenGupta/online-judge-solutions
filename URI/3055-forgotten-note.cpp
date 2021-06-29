f6929fb2a2bc0b1db5f71ef8df9131d3a5ab9033
SusmoySenGupta <susmoy.cse@gmail.com>
2021-06-29 19:38:16 +0600

    add 3055 solution

---
+++ URI/3055-forgotten-note.cpp
@@ -0,0 +1,23 @@
+/* 
+	problem no: 3055
+	problem name: Forgotten Note
+	problem link: https://www.urionlinejudge.com.br/judge/en/problems/view/3055
+	author: Susmoy Sen Gupta
+	Status: __Solved__
+	Solved at: 6/29/21, 7:36:41 PM
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	
+	cin >> a >> b;
+	
+	cout << (b * 2) - a << endl;
+	
+	return 0;
+}
