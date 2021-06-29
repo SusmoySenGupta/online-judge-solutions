fcb1860c699bbfa2ed5c5592fa78ea1d3f4838e1
SusmoySenGupta <susmoy.cse@gmail.com>
2021-06-29 18:59:50 +0600

    add 3037 solution

---
+++ URI/3037-playing-darts-by-distance.cpp
@@ -0,0 +1,42 @@
+/* 
+	problem no: 3037
+	problem name: Playing Darts by Distance
+	problem link: https://www.urionlinejudge.com.br/judge/en/problems/view/3037
+	author: Susmoy Sen Gupta
+	Status: ____
+	Solved at: __
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int t, x, d, john_score, mary_score;
+	
+	cin >> t;
+	
+	while(t--)
+	{
+		john_score = 0, mary_score = 0;
+		
+		for(int i = 0; i < 3; i++)
+		{
+			cin >> x >> d;
+			
+			john_score += (x * d); 
+		}
+		
+		for(int i = 0; i < 3; i++)
+		{
+			cin >> x >> d;
+			
+			mary_score += (x * d); 
+		}
+		
+		john_score > mary_score ? cout << "JOAO\n" : cout << "MARIA\n"; 	
+	}
+	
+	return 0;
+}
