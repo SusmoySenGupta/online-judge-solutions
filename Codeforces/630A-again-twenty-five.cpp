fd129ef869ce65d80e73113d6c73291db5b4b393
SusmoySenGupta <susmoy.cse@gmail.com>
2021-07-01 01:48:19 +0600

    add 630A solution

---
+++ Codeforces/630A-again-twenty-five.cpp
@@ -0,0 +1,39 @@
+/*
+	author: Susmoy Sen Gupta
+	email: susmoy.cse@gmail.com
+	github: github.com/SusmoySenGupta
+
+	Judge: Codeforces
+	problem no: 630A
+	problem name: Again Twenty Five!
+	problem link: https://codeforces.com/problemset/problem/630/A
+
+	Status: __Solved__
+	Solved at: Jul/01/2021 01:47
+*/
+
+#include <iostream>
+
+#define INF  (int)1e9
+#define EPS   1e-9
+#define MOD 1000000007ll
+#define PI 3.14159
+
+#define lli long long int
+
+#define rep(i, a, n) for (int i=a; i<n; i++)
+#define per(i, a, n) for (int i=n-1; i>=a; i--)
+
+using namespace std;
+
+int main()
+{
+	lli x;
+	
+	cin >> x;
+	
+	cout << 25 << endl;
+	
+    return 0;
+}
+
