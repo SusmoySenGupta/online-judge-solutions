b25b6b5a5f1877e5f4fc8ace7160c8508a51cf7e
SusmoySenGupta <susmoy.cse@gmail.com>
2021-07-02 12:47:46 +0600

    add 1352A solution

---
+++ Codeforces/1372A-omkar-and-completion.cpp
@@ -0,0 +1,58 @@
+/*
+	author: Susmoy Sen Gupta
+	email: susmoy.cse@gmail.com
+	github: github.com/SusmoySenGupta
+
+	Judge: Codeforces
+	problem no: 1372A
+	problem name: Omkar and Completion
+	problem link: https://codeforces.com/problemset/problem/1372/A
+
+	Status: __Solved__
+	Solved at: Jul/02/2021 12:46
+*/
+
+#include <iostream>
+
+#define INF  (int)1e9
+#define EPS   1e-9
+#define MOD 1000000007ll
+#define PI 3.14159
+
+#define lli long long int
+
+#define rep(i, a, n) for (int i=a; i<n; i++)
+#define per(i, a, n) for (int i=n-1; i>=a; i--)
+
+using namespace std;
+
+
+void solve()
+{
+	int n;
+	
+	cin >> n;
+	
+	while(n--)
+	{
+		cout << "1 ";
+	}
+	
+	cout << endl;
+}
+
+
+int main()
+{
+	int t;
+	
+	cin >> t;
+	
+	while(t--)
+	{
+		solve();
+	}
+	
+    return 0;
+}
+
