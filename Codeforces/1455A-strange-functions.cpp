911cee9c8a4327e0325e91cf0d4cb3fad27b38e2
SusmoySenGupta <susmoy.cse@gmail.com>
2021-07-06 19:20:32 +0600

    add 1455A solution

---
+++ Codeforces/1455A-strange-functions.cpp
@@ -0,0 +1,52 @@
+/*
+	author: Susmoy Sen Gupta
+	email: susmoy.cse@gmail.com
+	github: github.com/SusmoySenGupta
+
+	Judge: Codeforces
+	problem no: 1455A
+	problem name: Strange Functions
+	problem link: https://codeforces.com/problemset/problem/1455/A
+
+	Status: __Solved__
+	Solved at: Jul/06/2021 19:19
+*/
+
+#include <iostream>
+#include <cstring>
+
+#define INF  (int)1e9
+#define EPS   1e-9
+#define MOD 1000000007ll
+#define PI 3.14159
+
+#define lli long long int
+
+#define rep(i, a, n) for (int i=a; i<n; i++)
+#define per(i, a, n) for (int i=n-1; i>=a; i--)
+
+using namespace std;
+
+void solve()
+{
+	char n[102];
+	
+	scanf("%s", n);
+	
+	printf("%d\n", strlen(n));
+}
+
+int main()
+{
+	int t;
+	
+	scanf("%d", &t);
+	
+	while(t--)
+	{
+		solve();
+	}
+
+    return 0;
+}
+
