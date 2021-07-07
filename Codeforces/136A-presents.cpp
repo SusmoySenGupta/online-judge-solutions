b8a3fbb5f1cbe9c82c0a9946b3aeb0b404df9027
SusmoySenGupta <susmoy.cse@gmail.com>
2021-07-07 10:39:34 +0600

    add 136A solution

---
+++ Codeforces/136A-presents.cpp
@@ -0,0 +1,55 @@
+/*
+	author: Susmoy Sen Gupta
+	email: susmoy.cse@gmail.com
+	github: github.com/SusmoySenGupta
+
+	Judge: Codeforces
+	problem no: 136A
+	problem name: Presents
+	problem link: https://codeforces.com/problemset/problem/136/A
+
+	Status: __Solved__
+	Solved at: Jul/07/2021 10:33
+*/
+
+#include <iostream>
+
+#define INF  (int)1e9
+#define EPS   1e-9
+#define MOD 1000000007ll
+#define PI 3.14159
+
+#define lli long long int
+
+#define rep(i, a, n) for (int i=a; i<n; i++)
+#define per(i, a, n) for (int i=n-1; i>=a; i--)
+
+using namespace std;
+
+void solve()
+{
+	int n, x, arr[102];
+	
+	scanf("%d", &n);
+	
+	rep(i, 0, n)
+	{
+		scanf("%d", &x);
+		
+		arr[x - 1] = i + 1;
+	}
+	
+	rep(i, 0, n)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+int main()
+{
+	solve();
+
+    return 0;
+}
+
