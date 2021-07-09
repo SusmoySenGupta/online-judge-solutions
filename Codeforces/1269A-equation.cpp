e91427d3332d54e7707f1de4c6d29d8e1a0a92e3
SusmoySenGupta <susmoy.cse@gmail.com>
2021-07-10 01:26:23 +0600

    add 1269A solution

---
+++ Codeforces/1269A-equation.cpp
@@ -0,0 +1,43 @@
+/*
+	author: Susmoy Sen Gupta
+	email: susmoy.cse@gmail.com
+	github: github.com/SusmoySenGupta
+
+	Judge: Codeforces
+	problem no: 1269A
+	problem name: Equation
+	problem link: https://codeforces.com/problemset/problem/1269/A
+
+	Status: ____
+	Solved at: __
+*/
+
+#include <iostream>
+
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007ll
+#define PI 3.14159
+
+#define lli long long int
+
+#define rep(i, a, n) for (int i=a; i<n; i++)
+#define per(i, a, n) for (int i=n-1; i>=a; i--)
+
+using namespace std;
+
+void solve()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    printf("%d %d\n", 9 * n, 8 * n);
+}
+
+int main()
+{
+	solve();
+
+    return 0;
+}
