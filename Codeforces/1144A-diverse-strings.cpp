6812e88ed627c39302cbfc450bcfc45f1d7b78ff
SusmoySenGupta <susmoy.cse@gmail.com>
2021-07-09 22:13:36 +0600

    add 1144A solution

---
+++ Codeforces/1144A-diverse-strings.cpp
@@ -0,0 +1,65 @@
+/*
+	author: Susmoy Sen Gupta
+	email: susmoy.cse@gmail.com
+	github: github.com/SusmoySenGupta
+
+	Judge: Codeforces
+	problem no: 1144A
+	problem name: Diverse Strings
+	problem link: https://codeforces.com/problemset/problem/1144/A
+
+	Status: __Solved__
+	Solved at: Jul/09/2021 22:13
+*/
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007ll
+#define PI 3.14159
+#define MAX 102
+
+#define lli long long int
+
+#define rep(i, a, n) for (int i=a; i<n; i++)
+#define per(i, a, n) for (int i=n-1; i>=a; i--)
+
+using namespace std;
+
+bool isDiverse(char ch[])
+{
+    int len = strlen(ch);
+
+    sort(ch, ch + len);
+
+    for(int i=0; i < len - 1; i++)
+        if(ch[i+1] - ch[i] != 1)
+            return false;
+
+    return true;
+}
+
+void solve()
+{
+    int t;
+    char ch[MAX];
+
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        scanf("%s", ch);
+
+        isDiverse(ch) ? printf("YES\n") : printf("NO\n");
+    }
+}
+
+int main()
+{
+	solve();
+
+    return 0;
+}
