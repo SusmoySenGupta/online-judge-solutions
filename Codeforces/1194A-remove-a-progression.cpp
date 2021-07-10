2aee6587ad4da44e5e6dd854d3005f8727b41d8e
SusmoySenGupta <susmoy.cse@gmail.com>
2021-07-10 13:22:01 +0600

    add 1194A solution

---
+++ Codeforces/1194A-remove-a-progression.cpp
@@ -0,0 +1,48 @@
+/*
+	author: Susmoy Sen Gupta
+	email: susmoy.cse@gmail.com
+	github: github.com/SusmoySenGupta
+
+	Judge: Codeforces
+	problem no: 1194A
+	problem name: Remove a Progression
+	problem link: https://codeforces.com/problemset/problem/1194/A
+
+	Status: __Solved__
+	Solved at: Jul/10/2021 13:21
+*/
+
+#include <iostream>
+
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007ll
+#define PI 3.14159
+
+#define lli long long int
+
+#define rep(i, a, n) for (int i=a; i<n; i++)
+#define per(i, a, n) for (int i=n-1; i>=a; i--)
+
+using namespace std;
+
+void solve()
+{
+    int t, n, x;
+
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        scanf("%d%d", &n, &x);
+
+        printf("%d\n", x*2);
+    }
+}
+
+int main()
+{
+	solve();
+
+    return 0;
+}
