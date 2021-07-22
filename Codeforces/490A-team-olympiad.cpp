114e82fc24409f96926a4e8413046a59f47f6b37
SusmoySenGupta <susmoy.cse@gmail.com>
2021-07-22 11:16:41 +0600

    add 490A solution(better)

--- Codeforces/490A-team-olympiad.cpp
+++ Codeforces/490A-team-olympiad.cpp
@@ -9,7 +9,7 @@
 	problem link: https://codeforces.com/problemset/problem/490/A
 
 	Status: __Solved__
-	Solved at: Jul/22/2021 11:00
+	Solved at: Jul/22/2021 11:16
 */
 
 #include <iostream>
@@ -18,6 +18,7 @@
 #define EPS 1e-9
 #define MOD 1000000007ll
 #define PI 3.14159
+#define MAX 5003
 
 #define lli long long int
 
@@ -28,36 +29,27 @@ using namespace std;
 
 void solve()
 {
-    int n, arr[5003], freq[5]={0}, min_val;
+    int n, x, a[MAX], b[MAX], c[MAX], d, e, f, freq[5] = {0}, min_val;
 
     scanf("%d", &n);
 
+    d = e = f = 0;
+
     rep(i, 0, n) 
     {
-        scanf("%d", &arr[i]);
-        freq[arr[i]]++;
+        scanf("%d", &x);
+        freq[x]++;
+
+        if(x == 1)  a[d++] = i;
+        else if(x == 2) b[e++] = i;
+        else c[f++] = i;
     }
 
     min_val = min(min(freq[1], freq[2]), freq[3]);
 
     printf("%d\n", min_val);
 
-    rep(i, 0, min_val) 
-    {
-        rep(j, 1, 4)
-        {
-            rep(k, 0, n)
-            {
-                if(arr[k] == j)
-                {
-                    arr[k] = -1;
-                    printf("%d ", k+1);
-                    break;
-                }
-            }
-        }
-        printf("\n");
-    }
+    rep(i, 0, min_val) printf("%d %d %d\n", a[i]+1, b[i]+1, c[i]+1);
 }
 
 int main()
