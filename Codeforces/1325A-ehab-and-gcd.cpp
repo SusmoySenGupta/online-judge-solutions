2622b909a55c3e0075d1933ffe0b7374c05c8cc3
SusmoySenGupta <susmoy.cse@gmail.com>
2021-07-27 22:00:56 +0600

    add 1325A solution

---
+++ Codeforces/1325A-ehab-and-gcd.cpp
@@ -0,0 +1,47 @@
+/*
+	author: Susmoy Sen Gupta
+	email: susmoy.cse@gmail.com
+	github: github.com/SusmoySenGupta
+
+	Judge: Codeforces
+	problem no:
+	problem name:
+	problem link:
+
+	Status: __Solved__
+	Solved at: Jul/27/2021 21:25
+*/
+
+#include <iostream>
+
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007ll
+#define PI 3.14159
+
+#define lli long long int
+
+#define rep(i, a, n) for (int i=a; i<n; i++)
+#define per(i, a, n) for (int i=n-1; i>=a; i--)
+
+using namespace std;
+
+void solve()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    printf("1 %d\n", n-1);
+}
+
+int main()
+{   
+    int t;
+    
+    scanf("%d", &t);
+
+    while (t--) solve();
+
+    return 0;
+}
