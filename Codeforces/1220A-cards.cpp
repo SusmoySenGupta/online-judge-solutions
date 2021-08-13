09d1e9b6c039ecaf8caae51ef82b69d68a7e7199
SusmoySenGupta <susmoy.cse@gmail.com>
2021-08-13 10:09:16 +0600

    add 1220A solution

---
+++ Codeforces/1220A-cards.cpp
@@ -0,0 +1,58 @@
+/*
+	author: Susmoy Sen Gupta
+	email: susmoy.cse@gmail.com
+	github: github.com/SusmoySenGupta
+
+	Judge: Codeforces
+	problem no: 1220A
+	problem name: Cards
+	problem link: https://codeforces.com/problemset/problem/1220/A
+
+	Status: __Solved__
+	Solved at: Aug/13/2021 10:08
+*/
+
+#include <iostream>
+#include <algorithm>
+
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007ll
+#define PI 3.14159
+#define MAX 100005
+
+#define lli long long int
+
+#define rep(i, a, n) for (int i=a; i<n; i++)
+#define per(i, a, n) for (int i=n-1; i>=a; i--)
+
+using namespace std;
+
+void solve()
+{
+    int n, arr[MAX];
+    char ch[MAX];
+
+    scanf("%d %s", &n, ch);
+
+    int count_z = 0, count_n = 0, j = 0;
+    
+    rep(i, 0, n) 
+    {
+        if(ch[i] == 'z') arr[j++] = 0;
+        else if(ch[i] == 'n') arr[j++] = 1;
+    }
+
+    sort(arr, arr + j, greater<int>());
+
+    rep(i, 0, j) printf("%d ", arr[i]);
+
+    printf("\n");
+}
+
+int main()
+{
+	solve();
+
+    return 0;
+}
