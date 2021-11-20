f13bd1d81cf640a6b9eeec02af2a9e256c7f3d4c
SusmoySenGupta <susmoy.cse@gmail.com>
2021-11-20 19:04:14 +0600

    add two-sum solution

--- LeetCode/two-sum.cpp
+++ LeetCode/two-sum.cpp
@@ -1,22 +1,50 @@
-/* 
-    author: Susmoy Sen Gupta
-	problem link: https://leetcode.com/problems/two-sum/
-    Runtime: 4ms
-*/
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int len = nums.size();
-         vector<int> result;
-        for (int i = 0; i < len;i++) {
-            for (int j = i+1; j < len;j++){
-                if (nums[i] + nums[j] == target) {
-                    result = {i,j};
-                    return result;
-                }
-            }
-        }
+/*
+	author: Susmoy Sen Gupta
+	email: susmoy.cse@gmail.com
+	github: github.com/SusmoySenGupta
 
-        return result;
+	Judge: Leetcode
+	problem no: two-sum
+	problem name: Two Sum
+	problem link: https://leetcode.com/problems/two-sum
+
+	Tags: Array, Hashmap
+    
+    Technique, time & space complexity: 
+    Hashmap
+    Runtime: 12 ms, faster than 77.67% of C++ online submissions for Two Sum.
+    Memory Usage: 10.9 MB, less than 28.51% of C++ online submissions for Two Sum.
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    unordered_map<int, int> hash;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int index = target - nums[i];
+        if (hash.find(index) != hash.end())
+            return {hash[index], i};
+        hash[nums[i]] = i;
     }
-};
+
+    return {};
+}
+
+int main()
+{
+    vector<int> nums{2,7,11,15}, ans;
+    int target = 9;
+
+    ans = twoSum(nums, target);
+
+    printf("Output: [%d, %d]\n", ans[0], ans[1]);
+
+    return 0;
+}
